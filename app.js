c86c0317130ddf85be70f631d1591124a702ad88
sunkuangdong <sunkuangdong@edianzu.com>
2021-12-25 19:09:28 +0800

    初始化页面

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-const koa = require('koa');
+const Koa = require('koa');
+
+const app = new Koa();
+
+app.use(ctx => {
+    ctx.body = 'hello world'
+})
+
+// 监听本地3000端口
+app.listen(3000, () => {
+    console.log("server is running at http://localhost:3000");
+})
